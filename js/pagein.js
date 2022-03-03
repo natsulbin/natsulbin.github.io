f595dc2024399fa495c934a30c2450f2be45a67e
NatsuL <735088849@qq.com>
2022-03-03 22:23:44 +0800

    Site updated: 2022-03-03 22:23:43

--- js/pagein.js
+++ js/pagein.js
@@ -6,7 +6,7 @@ var vh=document.documentElement.clientHeight;
 window.onload = function() {
     var sUserAgent = navigator.userAgent;
     if (sUserAgent.indexOf('Android') > -1 || sUserAgent.indexOf('iPhone') > -1 || sUserAgent.indexOf('iPad') > -1 || sUserAgent.indexOf('iPod') > -1 || sUserAgent.indexOf('Symbian') > -1) {
-        for(let i=1;i<count;i++) {
+        for(let i=0;i<count;i++) {
             page[i].style.animation="pagein 0.5s ease-in";
             page[i].style.opacity="1";
         }
@@ -17,12 +17,13 @@ window.onload = function() {
                 page[i].style.animation="pagein 0.5s ease-in";
                 page[i].style.opacity="1";
             }
+        } else {
+            setTimeout(function() {
+                page[0].style.animation="pagein 0.5s ease-in";
+                page[0].style.opacity="1"
+            },"900");
         }
     }
-    setTimeout(function() {
-        page[0].style.animation="pagein 0.5s ease-in";
-        page[0].style.opacity="1"
-    },"990");
 }
 window.onscroll = function() {
     var fh=document.documentElement.scrollTop;
