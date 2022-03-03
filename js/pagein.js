80064a15d21d167ae39c558854e10cfb2ee3e77f
NatsuL <735088849@qq.com>
2022-03-03 18:15:56 +0800

    Site updated: 2022-03-03 18:15:56

--- js/pagein.js
+++ js/pagein.js
@@ -4,19 +4,31 @@ var count=page.length;
 var bh=document.documentElement.scrollHeight;
 var vh=document.documentElement.clientHeight;
 window.onload = function() {
-    var fh=document.documentElement.scrollTop;
-    if(fh+vh>=bh) {
-        for(let i=0;i<count;i++) {
+    var sUserAgent = navigator.userAgent;
+    if (sUserAgent.indexOf('Android') > -1 || sUserAgent.indexOf('iPhone') > -1 || sUserAgent.indexOf('iPad') > -1 || sUserAgent.indexOf('iPod') > -1 || sUserAgent.indexOf('Symbian') > -1) {
+        for(let i=1;i<count;i++) {
             page[i].style.animation="pagein 0.5s ease-in";
             page[i].style.opacity="1";
         }
+    } else {
+        var fh=document.documentElement.scrollTop;
+        if(fh+vh>=bh) {
+            for(let i=0;i<count;i++) {
+                page[i].style.animation="pagein 0.5s ease-in";
+                page[i].style.opacity="1";
+            }
+        }
     }
+    setTimeout(function() {
+        page[0].style.animation="pagein 0.5s ease-in";
+        page[0].style.opacity="1"
+    },"990");
 }
 window.onscroll = function() {
     var fh=document.documentElement.scrollTop;
-    for(let i=0;i<count;i++) {
+    for(let i=1;i<count;i++) {
         var distance=page[i].offsetTop - document.documentElement.scrollTop;
-        if(distance<500 || fh+vh>bh) {
+        if(distance<700 || fh+vh+100>=bh) {
             page[i].style.animation="pagein 0.5s ease-in";
             page[i].style.opacity="1";
         }
